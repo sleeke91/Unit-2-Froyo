583e90a6c410f5ec95aa9d071f5bfc8b8a1dbb69
Samantha Leeke <samantha@Samanthas-MacBook-Pro.local>
2025-05-07 21:47:38 -0500

    basic first attempt at workshop

---
+++ index.js
@@ -0,0 +1,20 @@
+// Can you prompt the user for froyo flavors and store their input?
+
+const userInputString = prompt(
+    "Please enter your flavors seperated by commas:",
+    "coffee, strawberry, chocolate, coffee, vanilla"
+  );
+
+// Can you parse the user input into an array of froyo flavors?
+
+const flavorArray = userInputString
+console.log(flavorArray)
+
+// Can you build an object to track which flavors you have counted so far?
+
+// How should that object be updated as you iterate through the array of flavors?
+
+// Are you using functions to organize your code?
+
+// Do your variables and functions have meaningful names?
+
