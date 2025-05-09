edae2f27e7b16a34471a06774ecef86d2f37526b
Samantha Leeke <samantha@Samanthas-MacBook-Pro.local>
2025-05-09 14:58:40 -0500

    actually figured it out

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 // Can you prompt the user for froyo flavors and store their input?
-
-const userInputString = prompt(
-    "Please enter your flavors seperated by commas:",
-    "coffee, strawberry, chocolate, coffee, vanilla"
-  );
-
-// Can you parse the user input into an array of froyo flavors?
-
-const flavorArray = userInputString
-console.log(flavorArray)
+const userOrder = prompt(
+  "Please enter your flavors seperated by commas:",
+  " coffee, strawberry, chocolate, coffee, vanilla, strawberry"
+);
+// turn the singular user inputted 'string' and tell it to split where there is a "," to give multiple items in my array
+const flavorArray = userOrder.split(",");
 
 // Can you build an object to track which flavors you have counted so far?
-
+const orderedFlavorsObject = (arr)=>{
+const myObject = {}
 // How should that object be updated as you iterate through the array of flavors?
-
-// Are you using functions to organize your code?
-
-// Do your variables and functions have meaningful names?
+for(let i=0;i<arr.length;i++){
+  const eachFlavor = arr[i]
+  const currentObjectKeys = Object.keys(myObject)
+  if(currentObjectKeys.includes(eachFlavor)){
+    myObject[eachFlavor] = myObject[eachFlavor]+1
+  }else{
+    myObject[eachFlavor] = 1
+  }
+}
+return myObject
+}
+console.log(orderedFlavorsObject(flavorArray))
 
